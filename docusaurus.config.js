3c53add7d69a68c8add478a80b2a98c334430811
cyhan <cyhan.dev@outlook.com>
2025-05-11 20:54:17 +0800

    repo: fix editUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,8 +55,7 @@ const config = {
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/sig-a11y/memo/tree/main/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
